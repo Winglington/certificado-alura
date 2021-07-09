db61aea96164e48786fca8c5e632195808d8b8c9
Josue Winglington <winglington@gmail.com>
2021-07-09 09:54:39 -0300

    Update scripts.js

--- conversordemoedas/scripts.js
+++ conversordemoedas/scripts.js
@@ -1,5 +1,5 @@
 var valorEmDollar = prompt("Digite o valor em dollar")
 valorEmDollar = parseFloat(valorEmDollar)
-var valorEmReal = valorEmDollar * 5.77
+var valorEmReal = valorEmDollar * 5.26
 valorEmReal = valorEmReal.toFixed(2)
 alert(valorEmReal)
